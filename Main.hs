e8bea7de479a088af66bfcaab25b35ba5cd2c630
Eric Mertens <emertens@gmail.com>
2013-11-03 14:03:35 -0800

    Add Username/Password, Bind, and UDP Association

--- Main.hs
+++ Main.hs
@@ -1,18 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Network.Socks5
 import Network.Socks5.Types
 import Network.Socks5.Lowlevel
-import Network.Socket hiding (recv)
+import Network.Socket hiding (recv, recvFrom, sendTo)
 import Network.Socket.ByteString
 import Control.Concurrent
+import Data.List (find)
 import Data.Foldable (for_)
+import Data.Serialize (encode,decode)
 import Control.Monad
 import Control.Exception
 import System.IO
 import System.IO.Error
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString as B
+import Data.ByteString (ByteString)
 
 ------------------------------------------------------------------------
 -- Configuration
@@ -24,6 +28,10 @@ data Configuration = Configuration
   , listenService :: ServiceName
   , debugLevel    :: Verbosity
   , logLock       :: MVar ()
+  , configUser    :: ByteString
+  , configPass    :: ByteString
+  , authPreference :: [SocksMethod]
+  , bindAddress   :: SockAddr
   }
 
 data Verbosity = VInfo | VDebug
@@ -31,7 +39,8 @@ data Verbosity = VInfo | VDebug
 
 getConfiguration = do
   logMutex <- newMVar ()
-  return (Configuration "::" "2080" VDebug logMutex)
+  return (Configuration "" "2080" VDebug logMutex "emertens" "paswerd" [SocksMethodUsernamePassword,SocksMethodNone]
+                        (SockAddrInet aNY_PORT iNADDR_ANY))
 
 ------------------------------------------------------------------------
 -- Logging
@@ -79,6 +88,11 @@ listenerLoop :: Configuration -> AddrInfo -> IO ()
 listenerLoop config ai =
   bracket (socket (addrFamily ai) (addrSocketType ai) (addrProtocol ai)) sClose $ \s ->
   do setSocketOption s ReuseAddr 1
+
+     -- We support binding on multiple addresses. Keeping them separate
+     -- will help later when we're making additional connections for UDP.
+     when (addrFamily ai == AF_INET6) (setSocketOption s IPv6Only 1)
+
      bind s (addrAddress ai)
      listen s maxListenQueue
      forever $ do
@@ -95,15 +109,33 @@ handleClientHello :: Configuration -> Socket -> SockAddr -> IO ()
 handleClientHello config s who = do
   debug config ("Client thread started for " ++ show who)
   SocksHello authTypes <- waitSerialized s
+
   debug config ("Client proposed " ++ show authTypes)
+  debug config ("Server supports " ++ show (authPreference config))
 
-  if SocksMethodNone `elem` authTypes
+  case find (`elem` authTypes) (authPreference config) of
 
-    then do debug config "No authentication selected"
+    Just SocksMethodNone ->
+         do debug config "No authentication selected"
             sendSerialized s (SocksHelloResponse SocksMethodNone)
             readyForClientRequest config s who
 
-    else do debug config "No acceptable authentication methods proposed"
+    Just SocksMethodUsernamePassword ->
+         do debug config "Username/password authentication selected"
+            sendSerialized s (SocksHelloResponse SocksMethodUsernamePassword)
+            login <- waitSerialized s
+
+            if configUser config == plainUsername login &&
+               configPass config == plainPassword login
+
+              then do debug config "Authentication succeeded"
+                      sendSerialized s SocksPlainLoginSuccess
+                      readyForClientRequest config s who
+
+              else do debug config "Authentication failed"
+                      sendSerialized s SocksPlainLoginFailure
+
+    _ -> do debug config "Authentication failed"
             sendSerialized s (SocksHelloResponse SocksMethodNotAcceptable)
 
 ------------------------------------------------------------------------
@@ -113,20 +145,21 @@ handleClientHello config s who = do
 
 readyForClientRequest :: Configuration -> Socket -> SockAddr -> IO ()
 readyForClientRequest config s who = do
-  SocksRequest cmd dst <- waitSerialized s
-  debug config ("Requesting " ++ show cmd ++ " @ " ++ show dst)
-  mbDst <- resolveSocksAddress config dst
+  req <- waitSerialized s
+  mbDst <- resolveSocksAddress config (requestDst req)
   case mbDst of
-    Nothing -> do info config "Connection failed"
-                  sendSerialized s (errorResponse SocksErrorHostUnreachable)
-    Just dstAddr -> handleClientRequest cmd config s who dstAddr
 
+    Nothing  -> do info config "Connection failed"
+                   sendSerialized s (errorResponse SocksErrorHostUnreachable)
+
+    Just dst -> do handleClientRequest (requestCommand req) config s who dst
 
 ------------------------------------------------------------------------
 -- Request modes
 ------------------------------------------------------------------------
 
 handleClientRequest :: SocksCommand -> Configuration -> Socket -> SockAddr -> SockAddr -> IO ()
+
 
 handleClientRequest SocksCommandConnect config s who dst =
   bracket (socket (sockAddrFamily dst) Stream defaultProtocol)
@@ -148,6 +181,69 @@ handleClientRequest SocksCommandConnect config s who dst =
       localAddr <- sockAddrToSocksAddress `fmap` getSocketName c
       sendSerialized s (SocksResponse SocksReplySuccess localAddr)
       tcpRelay config s c
+
+
+
+handleClientRequest SocksCommandBind config s who dst
+  = flip finally (info config "Thread Complete")
+  $ bracket (socket (sockAddrFamily dst) Stream defaultProtocol) sClose $ \c ->
+  do
+  debug config "Binding TCP socket"
+
+  bind c (bindAddress config)
+  listen c 0
+  boundAddr <- getSocketName c
+  info config ("Socket bound to " ++ show boundAddr)
+  sendSerialized s (SocksResponse SocksReplySuccess (sockAddrToSocksAddress boundAddr))
+
+  bracket (accept c) (sClose.fst) $ \(c1,who) ->
+    do debug config ("Connection received from " ++ show who)
+       sendSerialized s (SocksResponse SocksReplySuccess (sockAddrToSocksAddress who))
+       tcpRelay config s c1
+
+
+
+handleClientRequest SocksCommandUdpAssociate config s who dst =
+  flip finally (info config "Thread Complete") $
+  getSocketName s >>= \localAddr ->
+  bracket (socket (sockAddrFamily localAddr) Datagram defaultProtocol) sClose $ \c1 ->
+  bracket (socket (sockAddrFamily localAddr) Datagram defaultProtocol) sClose $ \c2 ->
+  do
+
+  debug config "Associating UDP socket"
+  debug config ("UDP destination " ++ show dst)
+
+  info config ("Trying to bind " ++ show (setPort 0 localAddr))
+  bind c1 (setPort 0 localAddr)
+  localDataAddr <- getSocketName c1
+  info config ("UDP incoming socket bound to " ++ show localDataAddr)
+
+  bind c2 (wildAddress (sockAddrFamily localAddr))
+  remoteDataAddr <- getSocketName c2
+  info config ("UDP outgoing socket bound to " ++ show remoteDataAddr)
+
+  sendSerialized s (SocksResponse SocksReplySuccess (sockAddrToSocksAddress localDataAddr))
+
+  -- XXX: Need to support the case where 'dst' is 0:0
+
+  _forwardThread  <- forkIO $ forever $ do
+                       (bs,src) <- recvFrom c1 4096
+                       case (src == dst, decode bs) of
+                         (True, Right udp) | udpFragment udp == 0 -> do
+                           debug config ("Got packet from client to " ++ show (udpRemoteAddr udp))
+                           mbAddr <- resolveSocksAddress config (udpRemoteAddr udp)
+                           for_ mbAddr (sendTo c2 (udpContents udp))
+                         _ -> debug config "Ignoring UDP packet"
+
+  _backwardThread <- forkIO $ forever $ do
+                       (msg, remote) <- recvFrom c2 4096
+                       debug config ("Got packet from remote " ++ show remote)
+                       sendTo c1 (encode (SocksUdpEnvelope 0 (sockAddrToSocksAddress remote) msg)) dst
+
+  -- UDP connections are preserved until the control connection goes down
+  setSocketOption s KeepAlive 1
+  recv s 1
+  return ()
 
 
 handleClientRequest cmd config s who _ = do
@@ -212,3 +308,17 @@ tcpHints = defaultHints
          { addrSocketType = Stream
          , addrFlags      = [AI_ADDRCONFIG]
          }
+
+setPort :: PortNumber -> SockAddr -> SockAddr
+setPort port (SockAddrInet  _      host      ) = SockAddrInet  port      host
+setPort port (SockAddrInet6 _ flow host scope) = SockAddrInet6 port flow host scope
+setPort _    SockAddrUnix {}                   = error "unix sockets don't have ports"
+
+getPort :: SockAddr -> PortNumber
+getPort (SockAddrInet  port   _  ) = port
+getPort (SockAddrInet6 port _ _ _) = port
+getPort SockAddrUnix {}            = error "seriously, stop using unix sockets"
+
+wildAddress :: Family -> SockAddr
+wildAddress AF_INET  = SockAddrInet aNY_PORT iNADDR_ANY
+wildAddress AF_INET6 = SockAddrInet6 aNY_PORT 0 iN6ADDR_ANY 0
